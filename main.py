371a323667bb9d11a97464cdf13a3af59f2d5b1c
guzel <guzel.kh23@gmail.ru>
2023-06-18 12:21:47 +0300

    global variable fixed

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from utils import get_data, get_executed_operations, get_last_operations, get_formatted_data
 
+COUNT_LAST_OPERATIONS = 3
+
 
 def main():
-    COUNT_LAST_OPERATIONS = 3
-    filename = 'operations.json'
-
-    data = get_data(filename)
+    data = get_data()
     data = get_executed_operations(data)
     data = get_last_operations(data, COUNT_LAST_OPERATIONS)
     data = get_formatted_data(data)
